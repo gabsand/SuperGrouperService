ede094a8a6ed7a03f663a42338e745bc12001520
Gabrielle Sanderson <Gabrielle.Sanderson@renaissance.com>
2017-02-08 21:21:48 -0600

    Added Group, Groupable, Partition models

---
+++ SuperGrouper/SuperGrouper/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SuperGrouper.Models
+{
+    public class Group
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Groupable> Members { get; set; }
+        public List<Partition> Partitions { get; set; }
+        public Partition ParentPartition { get; set; }
+    }
+}
---
+++ SuperGrouper/SuperGrouper/Models/Groupable.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperGrouper.Models
+{
+    public class Groupable
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? Value { get; set; }
+    }
+}
