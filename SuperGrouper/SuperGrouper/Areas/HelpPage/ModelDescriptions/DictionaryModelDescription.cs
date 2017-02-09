daf0da00beb2e258135abcd01aa233c5c1486bd1
Gabrielle Sanderson <Gabrielle.Sanderson@renaissance.com>
2017-02-08 20:16:35 -0600

    Create Web API project
    - installed Swagger and MongoDB Driver
    - created GroupController

---
+++ SuperGrouper/SuperGrouper/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SuperGrouper.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SuperGrouper/SuperGrouper/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SuperGrouper.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
