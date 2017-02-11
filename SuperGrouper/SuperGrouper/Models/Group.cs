f7a782e27b16085731be9303d0c8860bc40dfacd
Gabrielle Sanderson <Gabrielle.Sanderson@renaissance.com>
2017-02-11 11:52:20 -0600

    Added unit tests for GroupController GET and POST methods

--- SuperGrouper/SuperGrouper/Models/Group.cs
+++ SuperGrouper/SuperGrouper/Models/Group.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SuperGrouper.Models
 {
     public class Group
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Groupable> Members { get; set; }
