f45f1ab036f892ff5402d708bbe20751a9ba6e1e
Gabrielle Sanderson <Gabrielle.Sanderson@renaissance.com>
2017-02-14 08:13:15 -0600

    Modified tests to use validators

--- SuperGrouper/SuperGrouper.Tests/Controllers/GroupsControllerTests.cs
+++ SuperGrouper/SuperGrouper.Tests/Controllers/GroupsControllerTests.cs
@@ -7,19 +7,36 @@ using System.Threading.Tasks;
 using SuperGrouper.Models;
 using System.Web.Http.Results;
 using MongoDB.Bson;
+using SuperGrouper.Controllers.Validators;
+using SuperGrouper.Repositories;
+using FluentValidation;
+using System.Collections.Generic;
 
 namespace SuperGrouper.Tests.Controllers
 {
     [TestFixture]
     public class GroupsControllerTests
     {
+        public GroupsController GetGroupsController(IGroupRepository groupRepository = null, 
+            IValidator<string> objectIdValidator = null,
+            IValidator<Group> groupValidator = null, 
+            IValidator<List<Member>> membersValidator = null)
+        {
+            var mockGroupRepository = groupRepository ?? new Mock<IGroupRepository>().Object;
+            var mockObjectIdValidator = objectIdValidator ?? new Mock<IValidator<string>>().Object;
+            var mockGroupValidator = groupValidator ?? new Mock<IValidator<Group>>().Object;
+            var mockMembersValidator = membersValidator ?? new Mock<IValidator<List<Member>>>().Object;
+
+            return new GroupsController(mockGroupRepository, mockObjectIdValidator, mockGroupValidator, mockMembersValidator);
+        }
+
         [Test]
         public void GetGroup_InvalidObjectId_ReturnsBadRequest()
         {
             var groupId = "invalidObjectId";
             var groupRepository = new Mock<IGroupRepository>();
 
-            var sut = new GroupsController(groupRepository.Object);
+            var sut = GetGroupsController(groupRepository.Object);
 
             var actionResult = sut.GetGroup(groupId).Result;
             var contentResult = actionResult as BadRequestErrorMessageResult;
@@ -35,7 +52,7 @@ namespace SuperGrouper.Tests.Controllers
             groupRepository.Setup(x => x.GetGroup(It.IsAny<ObjectId>()))
                 .Returns(Task.FromResult<Group>(null));
 
-            var sut = new GroupsController(groupRepository.Object);
+            var sut = GetGroupsController(groupRepository.Object);
 
             var actionResult = sut.GetGroup(groupId.ToString()).Result;
             var contentResult = actionResult as NotFoundResult;
@@ -51,7 +68,7 @@ namespace SuperGrouper.Tests.Controllers
             groupRepository.Setup(x => x.GetGroup(It.IsAny<ObjectId>()))
                 .Returns(Task.FromResult<Group>(new Group() {Id = groupId}));
 
-            var sut = new GroupsController(groupRepository.Object);
+            var sut = GetGroupsController(groupRepository.Object);
 
             var actionResult = sut.GetGroup(groupId.ToString()).Result;
             var contentResult = actionResult as OkNegotiatedContentResult<Group>;
@@ -71,7 +88,7 @@ namespace SuperGrouper.Tests.Controllers
             groupRepository.Setup(x => x.SaveGroup(It.IsAny<Group>()))
                 .Returns(Task.FromResult<Group>(null));
 
-            var sut = new GroupsController(groupRepository.Object);
+            var sut = GetGroupsController(groupRepository.Object);
 
             var actionResult = sut.SaveGroup(group).Result;
             var contentResult = actionResult as InternalServerErrorResult;
@@ -91,7 +108,7 @@ namespace SuperGrouper.Tests.Controllers
             groupRepository.Setup(x => x.SaveGroup(It.IsAny<Group>()))
                 .Returns(Task.FromResult<Group>(group));
 
-            var sut = new GroupsController(groupRepository.Object);
+            var sut = GetGroupsController(groupRepository.Object);
 
             var actionResult = sut.SaveGroup(group).Result;
             var contentResult = actionResult as OkNegotiatedContentResult<Group>;
